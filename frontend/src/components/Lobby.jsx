6128c0e177fa2222264fa5d09d28172dfc72e63a
nuuttihenriksson <henriksson.nuutti@gmail.com>
2024-02-28 18:51:50 +0200

    added nav-bar and re-structured code

--- frontend/src/components/Lobby.jsx
+++ frontend/src/components/Lobby.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from 'react'
-import CreateRoomForm from './CreateRoomForm'
-import Room from './Room'
-import RoomList from './RoomList';
 import { Typography } from '@mui/material'
 import { useNavigate, useLocation } from "react-router-dom";
 
@@ -13,40 +10,23 @@ const Lobby = ({socket}) => {
   // GET USERNAME FROM INNER STATE!
   const location = useLocation()
   const navigate = useNavigate()
-  
+
   const username = location.state?.username
 
   useEffect(() => {
     
     if(!location.state) {
-      navigate("/")
+      navigate("/signup")
       return;
     }
 
-    socket.on('room-list', (data) => {
-      console.log("fetching room list: " + data)
-      setRoomList(data)
-    })
-
-    socket.emit('room-list', "hello ::DDDD")
-    return () => {
-      console.log("cleanup")
-      socket.off("room-list")
-    }
   }, [location.state, navigate, socket])
-
-  const handleJoinRoom = (roomName) => {
-    console.log("join room")
-    navigate(`/room/${roomName}`, { state: { username, roomName }})
-  }
   
   return (
     <>
       <Typography variant="h1">
-          Hello! {username}
+          Hello {username}! This is the frontpage.
       </Typography>
-      <RoomList roomList={roomList} handleJoinRoom={handleJoinRoom}></RoomList>
-      <CreateRoomForm handleCreateRoom={handleJoinRoom}></CreateRoomForm>
     </>
   )}
 
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react'
+import CreateRoomForm from './CreateRoomForm'
+import RoomList from './RoomList';
+import { useNavigate, useLocation, Outlet } from "react-router-dom";
+import { Grid } from '@mui/material';
+
+const NavBar = ({socket}) => {
+
+  const [roomList, setRoomList] = useState([]);
+
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const username = location.state?.username
+
+  useEffect(() => {
+    
+    if(!location.state) {
+      navigate("/signup")
+      return;
+    }
+
+    socket.on('room-list', (data) => {
+      console.log("fetching room list: " + data)
+      setRoomList(data)
+    })
+
+    socket.emit('room-list', "hello ::DDDD")
+    return () => {
+      console.log("cleanup")
+      socket.off("room-list")
+    }
+  }, [location.state, navigate, socket])
+
+  const handleJoinRoom = (roomName) => {
+    console.log("join room")
+    navigate(`/room/${roomName}`, { state: { username, roomName }})
+  }
+
+  return (
+    <>
+    <Grid container gap={6} spacing={1} sx={{height: '100%' }}>
+      <Grid item xs={2.5} sx={{backgroundColor: 'lightgray', padding: '30px'}}>
+        <CreateRoomForm handleCreateRoom={handleJoinRoom}></CreateRoomForm>
+        <RoomList roomList={roomList} handleJoinRoom={handleJoinRoom}></RoomList>
+      </Grid>
+      <Grid item xs={8.5}>
+        <Outlet />
+      </Grid>
+    </Grid>
+    </>
+  )
+
+}
+
+export default NavBar;
